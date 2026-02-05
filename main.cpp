5673af48db27725cb81b8e5b4ff0b3ab6e55eacf
dracula55988 <80603365+dracula55988@users.noreply.github.com>
2026-02-05 18:54:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,32 +3,53 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <android/log.h>
+#include <dlfcn.h>
 
-#define LOG_TAG "IDFixer"
+#define LOG_TAG "VehicleLimitFixer"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-// Memory utility to bypass Android's write protection
-void patch_memory(uintptr_t addr, const char* data, size_t size) {
+// Utility to find the base address of a loaded library
+uintptr_t get_lib_base(const char* libName) {
+    FILE* fp = fopen("/proc/self/maps", "r");
+    char line[512];
+    uintptr_t base = 0;
+    while (fgets(line, sizeof(line), fp)) {
+        if (strstr(line, libName)) {
+            base = (uintptr_t)strtoull(line, NULL, 16);
+            break;
+        }
+    }
+    fclose(fp);
+    return base;
+}
+
+// Memory patcher that handles write protection
+void patch_mem(uintptr_t addr, const char* data, size_t size) {
     uintptr_t page_start = addr & ~(getpagesize() - 1);
     mprotect((void*)page_start, getpagesize() * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
     memcpy((void*)addr, data, size);
     mprotect((void*)page_start, getpagesize() * 2, PROT_READ | PROT_EXEC);
 }
 
-// MonetLoader entry point
 extern "C" jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-    LOGI("Plugin Loading: Bypassing Vehicle ID Limits...");
+    LOGI("Plugin Loading: Preparing to patch Vehicle ID Limits...");
 
-    // Find the address of libGTASA.so in memory
-    // In your crash log, the crash was at libGTASA.so + 0x45B3C8
-    // We will NOP (disable) the check that causes the null pointer crash.
+    // Wait a moment for the game library to be fully loaded
+    uintptr_t gtasa_base = get_lib_base("libGTASA.so");
     
-    // 0x1F 0x20 0x03 0xD5 is the ARM64 instruction for "NOP" (Do Nothing)
-    // You'll need the base address of the library, which MonetLoader usually provides.
-    // For this example, we assume we're patching the offset directly if we find the base.
-    
-    // (Actual logic would involve using dlopen/dlsym to find the library base)
-    
+    if (gtasa_base != 0) {
+        LOGI("Found libGTASA.so at: 0x%lx", gtasa_base);
+
+        // Crash site from your log: 0x45B3C8
+        // We are going to NOP the instruction that leads to the Null Pointer crash.
+        // ARM64 NOP instruction: 0x1F 0x20 0x03 0xD5
+        uintptr_t crash_addr = gtasa_base + 0x45B3C8;
+        patch_mem(crash_addr, "\x1F\x20\x03\xD5", 4);
+        
+        LOGI("Successfully patched vehicle limit check at 0x%lx", crash_addr);
+    } else {
+        LOGI("Error: Could not find libGTASA.so base address!");
+    }
+
     return JNI_VERSION_1_6;
 }
-
